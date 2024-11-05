53aaef40eac5122cbdf5f4310345dabaa1a365bf
Matheus <bonecodoido339@gmail.com>
2024-11-05 17:00:30 -0300

    Arrumar Erros do Update

---
+++ Migrations/20241105195339_ImageFix.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VimaV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImageFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Imagens",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sobrenome",
+                table: "Contatos",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contatos",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Contatos",
+                type: "varchar(500)",
+                maxLength: 500,
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Assunto",
+                table: "Contatos",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Imagens",
+                table: "Produtos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sobrenome",
+                table: "Contatos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contatos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Contatos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldMaxLength: 500)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Assunto",
+                table: "Contatos",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_general_ci");
+        }
+    }
+}
